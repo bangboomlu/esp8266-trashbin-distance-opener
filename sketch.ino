a5090f5a15238ca469c32e039cf46e8cca44d842
bangboomlu <lucas.hesse.reinsdorf@googlemail.com>
2020-07-15 14:34:28 +0200

    test image link

--- sketch.ino
+++ sketch.ino
@@ -8,8 +8,8 @@ const int echoPin = D1; // <-|HC distance sensor
 const int servoPin = D2;// <-|drive pin of servo
 
 
-int pulselength_oneRotation_left = 1280;// <-|these two values are depending on your servo. For me 1000 Microseconds is not turning ar all
-int pulselength_oneRotation_right = 760;// <-|less than 1000 turns the servo right and more is turning left. The further the value away from 1000 the faster it turns.
+int pulselength_oneRotation_ccw_perSecond = 1565;// <-|these two values are depending on your servo. For the FS90R 1500 Microseconds should be not turning at all
+int pulselength_oneRotation_cw_perSecond = 1365;// <-|less than 1500 turns the servo right and more is turning left. The further the value away from 1500 the faster it turns.
 long duration=0;
 long distance;
 int i;
@@ -43,13 +43,13 @@ void loop()
     Serial.print(distance); //print distance to Serial Port
     if (distance >= 20 && state!=1) // if distance >= 20cm turn servo one rotation left
         {
-            myservo.writeMicroseconds(pulselength_oneRotation_left);// this value is depending on your servo
+            myservo.writeMicroseconds(pulselength_oneRotation_ccw_perSecond);// this value is depending on your servo
             delay(1000);
             state=1;
         }
     else if (distance <= 21 && state!=2)
         {
-            myservo.writeMicroseconds(pulselength_oneRotation_right);
+            myservo.writeMicroseconds(pulselength_oneRotation_cw_perSecond);
             delay(1000);
             state=2;
         }
